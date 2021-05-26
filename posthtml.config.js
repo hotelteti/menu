99452ac164da9f4e5c8e0f6186ff59ff7185ffe8
Federico <me@fregante.com>
2021-05-27 02:37:14 +0700

    Update editor

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,22 +1,5 @@
-function cleanURLs(json) {
-	for (const section of json.Sezione) {
-		for (const dish of section.Piatto || section.Bevanda) {
-			if (dish.Foto) {
-				dish.Foto = dish.Foto.replace(/^.*[a-f0-9]{40}\/source\//, '');
-			}
-		}
-	}
-	return json;
-}
-
 module.exports = {
 	"plugins": {
-		"posthtml-expressions": {
-			"locals": {
-				cibo: cleanURLs(require('./cibo.json')),
-				bevande: cleanURLs(require('./bevande.json'))
-			}
-		},
 		"posthtml-img-autosize": {
 			"processEmptySize": true,
 			"root": "source"
--- pug.config.js
+++ pug.config.js
@@ -1,10 +1,6 @@
-const cibo = require('./cibo.json');
-const bevande = require('./bevande.json');
-cibo.Sezione = [...cibo.Sezione, ...bevande.Sezione];
-
 module.exports = {
   locals: {
-		lista: cibo,
+		lista: require('./lista.json'),
 		slugify: text => text.replace(/[^a-z]/gi, '').toLowerCase(),
 		relativizeCDNURL: url => url.replace(/^.*[a-f0-9]{40}\/source\//, ''),
 		lineBreakToBr: text => text.replace(/\n\n/g, '</p><p>').replace(/\n/g, '<br>')
