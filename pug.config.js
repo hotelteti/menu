5f9c4fe09d854bf7d54470907cb0a2732a604888
Federico <me@fregante.com>
2021-06-05 15:06:30 +0700

    WIP

--- pug.config.js
+++ pug.config.js
@@ -1,5 +1,20 @@
 module.exports = {
 	locals: {
+		opzioni: [
+			'Anidride solforosa o solfiti',
+			'Arachidi',
+			'Crostacei',
+			'Frutta a guscio',
+			'Glutine',
+			'Latte e derivati',
+			'Molluschi',
+			'Pesce',
+			'Sedano',
+			'Uova e derivati',
+			'Vegano',
+			'Vegetariano',
+			'Bio'
+		],
 		lista: require('./lista.json'),
 		slugify: (text = '') => text.replace(/[^a-z]/gi, '').toLowerCase(),
 		relativizeCDNURL: (url = '') => url.replace(/^.*[a-f0-9]{40}\/source\//, ''),
