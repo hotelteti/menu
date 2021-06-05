447551c2e2392afd308615589e9a57e87ee8084f
Federico <me@fregante.com>
2021-06-05 17:07:16 +0700

    WIP 2

--- pug.config.js
+++ pug.config.js
@@ -1,20 +1,24 @@
 module.exports = {
 	locals: {
-		opzioni: [
-			'Anidride solforosa o solfiti',
-			'Arachidi',
-			'Crostacei',
-			'Frutta a guscio',
-			'Glutine',
-			'Latte e derivati',
-			'Molluschi',
-			'Pesce',
-			'Sedano',
-			'Uova e derivati',
-			'Vegano',
-			'Vegetariano',
-			'Bio'
-		],
+		tags: {
+			Allergeni: [
+				'Anidride solforosa o solfiti',
+				'Arachidi',
+				'Crostacei',
+				'Frutta a guscio',
+				'Glutine',
+				'Latte e derivati',
+				'Molluschi',
+				'Pesce',
+				'Sedano',
+				'Uova e derivati',
+			],
+			Etichette: [
+				'Vegano',
+				'Vegetariano',
+				'Bio',
+			],
+		},
 		lista: require('./lista.json'),
 		slugify: (text = '') => text.replace(/[^a-z]/gi, '').toLowerCase(),
 		relativizeCDNURL: (url = '') => url.replace(/^.*[a-f0-9]{40}\/source\//, ''),
