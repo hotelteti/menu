5865c5c6e665eeb4f9265fdbb706875cec1fabe2
Federico <me@fregante.com>
2021-06-05 17:53:16 +0700

    Prettier

--- pug.config.js
+++ pug.config.js
@@ -21,7 +21,8 @@ module.exports = {
 		},
 		lista: require('./lista.json'),
 		slugify: (text = '') => text.replace(/[^a-z]/gi, '').toLowerCase(),
-		relativizeCDNURL: (url = '') => url.replace(/^.*[a-f0-9]{40}\/source\//, ''),
+		relativizeCDNURL: (url = '') =>
+			url.replace(/^.*[a-f0-9]{40}\/source\//, ''),
 		lineBreakToBr: (text = '') =>
 			text.replace(/\n\n/g, '</p><p>').replace(/\n/g, '<br>'),
 	},
