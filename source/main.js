a24010ddb1066bbccead719c00b40678e409a759
Federico <me@fregante.com>
2021-06-20 01:47:38 +0700

    Animate images opening

--- source/main.js
+++ source/main.js
@@ -1,3 +1,5 @@
+import animate from 'animate-prop';
+
 const $ = s => document.querySelector(s);
 const $$ = s => document.querySelectorAll(s);
 window.addEventListener('popstate', event => {
@@ -12,9 +14,38 @@ window.addEventListener('popstate', event => {
 	}
 });
 
-function zoomOnClick(event) {
-	event.currentTarget.disabled = true;
-	event.currentTarget.firstChild.style.height = 'auto';
+function easeOutQuint(x) {
+	return 1 - Math.pow(1 - x, 5);
+	}
+
+function zoomOnClick({currentTarget: button}) {
+	button.disabled = true;
+	const image = button.firstChild;
+	try {
+		const initialHeight = getComputedStyle(image).height;
+		image.style.height = 'auto';
+		const openHeight = getComputedStyle(image).height;
+		const {top} = image.getBoundingClientRect();
+		image.animate([
+			{height: initialHeight},
+			{height: openHeight}
+		], {
+			easing: 'cubic-bezier(0.22, 1, 0.36, 1)',
+			duration: 500
+		}).finished.then(() => {
+			image.style.height = 'auto';
+		});
+		animate(
+			document.documentElement,
+			'scrollTop',
+			document.documentElement.scrollTop + top - 100,
+			500,
+			easeOutQuint,
+		);
+	} catch (error) {
+		image.style.height = 'auto';
+		throw error;
+	}
 }
 
 $$('.dish-image').forEach(button => {
